649801fc7ed2de9e4216b0a41c48bbe22c0ce2eb
Mark Pokorny <iridium193@gmail.com>
2015-03-11 17:57:44 +0000

    Updated Exercise 5 with Jade’s solutions.

--- 05_silly_blocks/silly_blocks.rb
+++ 05_silly_blocks/silly_blocks.rb
@@ -1,3 +1,22 @@
+#Jade & Mark
+def reverser
+	word_list = yield.split(' ')
+	word_list.map do |word|
+		word.reverse
+	end.join(' ')
+
+end
+
+def adder(number = 1)
+	yield + number
+end
+
+def repeater(number_of_times = 1)
+	number_of_times.times {yield}
+end
+
+# Dan & Mark
+=begin
 def reverser
   yield.split(' ').map(&:reverse).join(' ')
 end
@@ -9,3 +28,4 @@ end
 def repeater(num=1)
   num.times {yield}
 end
+=end
