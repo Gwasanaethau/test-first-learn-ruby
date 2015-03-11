9194dd82f6467c20613a5d9ba975bd4616175383
Mark Pokorny <iridium193@gmail.com>
2015-03-11 17:57:29 +0000

    Updated Exercise 4 with Jade’s solutions.

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,3 +1,36 @@
+# Jade & Mark
+def translate(word)
+  vowels = /[aeiou]/
+  word_list = word.split(' ')
+  a = 'ay'
+  butchered_word_list = []
+  word_list.each do |other_word|
+    character_list = other_word.split('')
+    if (character_list[0] =~ vowels) == 0
+      butchered_word_list << other_word + a
+    elsif (character_list[0] =~ vowels) == nil
+      count = 0
+      character_list.each do |single|
+        if single == 'q'
+          count += 2
+           break
+        elsif (single =~ vowels) == nil
+          count += 1
+        else
+          break
+        end
+      end
+      first_part = character_list.slice(0...count).join
+      second_part = character_list.slice(count..character_list.length - 1).join
+      butchered_word = second_part + first_part + a
+      butchered_word_list << butchered_word
+    end
+  end
+  return butchered_word_list.join(' ')
+end
+
+# Dan & Mark
+=begin
 def translate(words)
   vowels = %w{a e i o u}
   words.split(" ").map do |word|
@@ -16,3 +49,4 @@ def translate(words)
     end
   end.join(" ")
 end
+=end
